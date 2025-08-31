0bf13ac36083a0994d318538e31146e8708e4422
Manoj Nepali <68263357+manoj2244@users.noreply.github.com>
2025-08-31 21:10:20 +0545

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,7 +10,7 @@ router.use(protect);
 router.get("/", authorize(["admin","super_admin","user"]), getUsers);
 router.get("/:id", authorize(["admin","super_admin"]), getUser);
 router.post("/", authorize(["super_admin"]), createUser);
-router.put("/:id", authorize(["admin","super_admin"]), updateUser);
+router.put("/:id", authorize(["admin","super_admin","user"]), updateUser);
 router.delete("/:id", authorize(["super_admin"]), deleteUser);
 
 module.exports = router;
