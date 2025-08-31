5c937cf925e6f9e3c88d36735530d2fb26eb3381
Manoj Nepali <68263357+manoj2244@users.noreply.github.com>
2025-08-31 20:26:31 +0545

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const path = require("path");
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
@@ -14,6 +16,12 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
